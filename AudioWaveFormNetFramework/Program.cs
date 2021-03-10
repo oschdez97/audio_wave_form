63c97ceb6372043d8bec310d33d45d361ba9d775
oschdez97 <oschdez97@gmail.com>
2021-03-10 18:57:15 +0100

    Initial

---
+++ AudioWaveFormNetFramework/Program.cs
@@ -0,0 +1,21 @@
+﻿using NAudio.WaveFormRenderer;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaveFormRendererLib;
+
+namespace AudioWaveFormNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string audioPath = @"C:\Users\root\Desktop\Tomorrow Is A Long Time.mp3";
+            string storePath = @"C:\Users\root\Desktop\mywave.png";
+            CreateAudioWaveForm.Create(audioPath, storePath);
+        }
+    }
+}
---
+++ WaveFormRendererLib/CreateAudioWaveForm.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.WaveFormRenderer;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace WaveFormRendererLib
+{
+    public static class CreateAudioWaveForm
+    {
+        public static void Create(string audioPath, string storePath)
+        {
+            MaxPeakProvider maxPeakProvider = new MaxPeakProvider();
+            RmsPeakProvider rmsPeakProvider = new RmsPeakProvider(200); // e.g. 200
+            SamplingPeakProvider samplingPeakProvider = new SamplingPeakProvider(200); // e.g. 200
+            AveragePeakProvider averagePeakProvider = new AveragePeakProvider(4); // e.g. 4
+
+            StandardWaveFormRendererSettings myRendererSettings = new StandardWaveFormRendererSettings();
+            myRendererSettings.Width = 1080;
+            myRendererSettings.TopHeight = 64;
+            myRendererSettings.BottomHeight = 64;
+
+            WaveFormRenderer renderer = new WaveFormRenderer();
+            var image = renderer.Render(audioPath, averagePeakProvider, myRendererSettings);
+            image.Save(storePath, ImageFormat.Png);
+        }
+    }
+}
